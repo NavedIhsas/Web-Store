85aec7d7265dcd1b9533def3525d2c8c9ab333ff
Navid <NavedAhsas77@gmail.com>
2023-05-31 16:06:46 +0330

    rename and update database

---
+++ SaleInShop/Domain/ShopModels/ProductProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.ShopModels;
+
+public partial class ProductProperty
+{
+    public Guid Id { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public string Value { get; set; }
+
+    public Guid? PropertyId { get; set; }
+
+    public virtual Property Property { get; set; }
+}
---
+++ SaleInShop/Domain/ShopModels/Property.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.ShopModels;
+
+public partial class Property
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public bool? IsSpecial { get; set; }
+}
